6c0960f768e2b8934e58cb1cb6cdb06c8807cdf3
똥망겜 <qnpfrebfqeifqemlfq@gmail.com>
2026-06-15 21:39:20 +0900

    Add static folder to Flask app configuration

--- api/index.py
+++ api/index.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.abspath(os.path.join(BASE_DIR, '../templates'))
+STATIC_DIR = os.path.abspath(os.path.join(BASE_DIR, '../static')
 
-app = Flask(__name__, template_folder=TEMPLATE_DIR)
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
 @app.route("/")
 def index():
