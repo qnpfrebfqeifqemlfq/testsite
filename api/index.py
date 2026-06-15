cd583994e85d3658194dd5908a52faf5620f4e0a
똥망겜 <qnpfrebfqeifqemlfq@gmail.com>
2026-06-15 21:27:30 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("../templates/index.html")
 
 @app.route("/clock")
 def clock():
-    return render_template("clock.html", target="2026-05-25T16:36:50")
+    return render_template("../templates/clock.html", target="2026-05-25T16:36:50")
 
 if __name__ == "__main__":
     app.run(debug=True)
