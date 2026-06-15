966239dc27043e43b42d43666fcb4eafadbfb00d
똥망겜 <qnpfrebfqeifqemlfq@gmail.com>
2026-06-15 21:48:43 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory("static", "favicon.ico")
+
 @app.route("/")
 def index():
     return render_template("index.html")
